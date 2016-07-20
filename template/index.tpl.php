22071cea067122138cf320a520074b1f32d42440
Vincen's <vincen.wen@samesamechina.com>
2016-07-20 10:57:35 +0800

    no message

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -271,6 +271,26 @@
 
 
 
+    /* 图片加载 */
+    function LoadFn ( arr , fn , fn2){
+        var loader = new PxLoader();
+        for( var i = 0 ; i < arr.length; i ++)
+        {
+            loader.addImage(arr[i]);
+        };
+
+        loader.addProgressListener(function(e) {
+            var percent = Math.round( e.completedCount / e.totalCount * 100 );
+            if(fn2) fn2(percent)
+        });
+
+
+        loader.addCompletionListener( function(){
+            if(fn) fn();
+        });
+        loader.start();
+    }
+
 
 
     var LoadingImg = [
